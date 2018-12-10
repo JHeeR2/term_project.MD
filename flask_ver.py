67596d5f380472cabf620570a966538fd358ce31
장희란 <44744255+JHeeR2@users.noreply.github.com>
2018-12-10 11:42:10 +0900

    Update flask_ver.py

--- flask_ver.py
+++ flask_ver.py
@@ -5,6 +5,7 @@ app=Flask(__name__)
 
 @app.route("/")
 
+
 def timeline():
     r=requests.get('https://ko.wikipedia.org/wiki/%ED%95%9C%EA%B5%AD%EC%82%AC_%EC%97%B0%ED%91%9C')
     r.encoding='utf-8'
@@ -15,17 +16,60 @@ def timeline():
     d=data[begin:end]    
     d=re.sub('<.+?>','',d,0)
     k=d.split('\n')
-    trash=['50년','100년','150년','200년','250년','300년','350년','400년','450년','500년','550년','600년','650년','700년',
-       '750년','800년','850년','900년','936년','950년','1000년','1050년','1100년','1150년','1200년','1250년','1300년',
-       '1350년','1392년','1400년','1450년','1500년','1550년','1600년','1650년','1700년','1750년','1800년','1850년',
-       '1875년','1897년','1900년','1910년','1920년','1930년','1940년']
-    for x in trash:
-        k.remove(x)
+    k.remove('50년')
+    k.remove('100년')
+    k.remove('150년')
+    k.remove('200년')
+    k.remove('250년')
+    k.remove('300년')
+    k.remove('350년')
+    k.remove('400년')
+    k.remove('450년')
+    k.remove('500년')
+    k.remove('550년')
+    k.remove('600년')
+    k.remove('650년')
+    k.remove('700년')
+    k.remove('750년')
+    k.remove('800년')
+    k.remove('850년')
+    k.remove('900년')
+    k.remove('936년')
+    k.remove('950년')
+    k.remove('1000년')
+    k.remove('1050년')
+    k.remove('1100년')
+    k.remove('1150년')
+    k.remove('1200년')
+    k.remove('1250년')
+    k.remove('1300년')
+    k.remove('1350년')
+    k.remove('1392년')
+    k.remove('1400년')
+    k.remove('1450년')
+    k.remove('1500년')
+    k.remove('1550년')
+    k.remove('1600년')
+    k.remove('1650년')
+    k.remove('1700년')
+    k.remove('1750년')
+    k.remove('1800년')
+    k.remove('1850년')
+    k.remove('1875년')
+    k.remove('1897년')
+    k.remove('1900년')
+    k.remove('1910년')
+    k.remove('1920년')
+    k.remove('1930년')
+    k.remove('1940년')
+    data=tuple(k)
     name=request.args.get('name')
-    num=0 
-    for i in k:
-        if name in i:
-            return i
+    num=0
+    l=len(str(name))
+    for i in data:
+        if str(name) in i[0:l]:
             num+=1
-        if num==0:    
-            return '해당 년도에 일어난 사건이 없습니다.'
+            return print (str(i))
+    if num==0:
+        return '해당 년도에 일어난 사건이 없습니다.'
+print('start', flush=True)
